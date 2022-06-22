ad806458743d4f2c7f604583dcac0be3ecba9ae3
Kuba Płaskonka <kuba.plaskonka@gmail.com>
2022-06-22 11:53:59 +0200

    Clippy

--- src/flipper.rs
+++ src/flipper.rs
@@ -1,13 +1,4 @@
-#[cfg(test)]
-use odra::instance::Instance;
-use odra::types::bytesrepr::Bytes;
-#[cfg(test)]
-use odra::types::bytesrepr::FromBytes;
-#[cfg(test)]
-use odra::types::bytesrepr::ToBytes;
-#[cfg(test)]
-use odra::types::{Address, RuntimeArgs};
-use odra::{TestEnv, Variable};
+use odra::Variable;
 
 #[odra::contract]
 pub struct Flipper {
@@ -32,64 +23,6 @@ impl Flipper {
     pub fn get(&self) -> bool {
         self.value.get_or_default()
     }
-
-    pub fn forty_two(&self) -> u32 {
-        42
-    }
-}
-
-#[cfg(test)]
-pub struct FlipperRef {
-    pub address: Address,
-}
-
-#[cfg(test)]
-impl FlipperRef {
-    pub fn flip(&self) {
-        TestEnv::call_contract(&self.address, "flip", &RuntimeArgs::new());
-    }
-
-    pub fn get(&self) -> bool {
-        let raw_result = TestEnv::call_contract(&self.address, "get", &RuntimeArgs::new());
-        <bool as FromBytes>::from_vec(raw_result.to_vec())
-            .unwrap()
-            .0
-    }
-
-    pub fn cross_flip(&self, addr: Address) {
-        TestEnv::call_contract(&addr, "flip", &RuntimeArgs::new());
-    }
-
-    pub fn forty_two(&self) -> u32 {
-        let raw_result = TestEnv::call_contract(&self.address, "forty_two", &RuntimeArgs::new());
-        <u32 as FromBytes>::from_vec(raw_result.to_vec()).unwrap().0
-    }
-}
-
-#[cfg(test)]
-impl Flipper {
-    fn deploy(name: &str) -> FlipperRef {
-        let mut container = odra_test_env::ContractContainer {
-            name: name.to_string(),
-            wasm_path: "flipper.wasm".to_string(),
-            entrypoints: Default::default(),
-        };
-        container.add("flip".to_string(), |name, _| {
-            Flipper::instance(name.as_str()).flip();
-            Bytes::new()
-        });
-
-        container.add("get".to_string(), |name, _| {
-            Flipper::instance(name.as_str())
-                .get()
-                .to_bytes()
-                .unwrap()
-                .into()
-        });
-
-        let address = TestEnv::register_contract(&container);
-        FlipperRef { address }
-    }
 }
 
 #[cfg(test)]
@@ -99,33 +32,19 @@ mod tests {
     #[test]
     fn flipping() {
         let contract = Flipper::deploy("flipper1");
-        assert_eq!(contract.get(), false);
+        assert!(!contract.get());
         contract.flip();
-        assert_eq!(contract.get(), true);
+        assert!(contract.get());
     }
 
     #[test]
     fn test_two_flippers() {
         let contract1 = Flipper::deploy("flipper1");
         let contract2 = Flipper::deploy("flipper2");
-        assert_eq!(contract1.get(), false);
-        assert_eq!(contract2.get(), false);
+        assert!(!contract1.get());
+        assert!(!contract2.get());
         contract1.flip();
-        assert_eq!(contract1.get(), true);
-        assert_eq!(contract2.get(), false);
-    }
-
-    #[test]
-    fn test_cross_flip() {
-        let contract1 = Flipper::deploy("flipper1");
-        let contract2 = Flipper::deploy("flipper2");
-        assert_eq!(contract1.get(), false);
-        assert_eq!(contract2.get(), false);
-        contract1.cross_flip(contract2.address.clone());
-        assert_eq!(contract1.get(), false);
-        assert_eq!(contract2.get(), true);
-        contract2.cross_flip(contract1.address.clone());
-        assert_eq!(contract1.get(), true);
-        assert_eq!(contract2.get(), true);
+        assert!(contract1.get());
+        assert!(!contract2.get());
     }
 }
